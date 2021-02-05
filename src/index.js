ee161233e5273edae9aefd0c2359617c885035ca
Pavel Zapletal <Pavel.Zapletal01@dixonscarphone.com>
2021-02-05 10:43:27 +0100

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import { newGame, throwedPins, getActualState } from "./bowlingScorer.js";
+// import { newGame, throwedPins, getActualState } from "./bowlingScorer.js";
 
-newGame();
-throwedPins(1);
-throwedPins(6);
-throwedPins(6);
-throwedPins(6);
-throwedPins(10);
-throwedPins();
-console.log(getActualState());
+console.log("string");
+// newGame();
+// throwedPins(1);
+// // throwedPins(6);
+// // throwedPins(6);
+// // throwedPins(6);
+// // throwedPins(10);
+// // throwedPins();
+// console.log(getActualState());
