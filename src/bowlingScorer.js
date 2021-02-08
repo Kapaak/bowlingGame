698d9f07d4d8bc0b3c5728de11cf0dda635ff2e7
Pavel Zapletal <Pavel.Zapletal01@dixonscarphone.com>
2021-02-08 09:54:25 +0100

    updated adding tables

--- src/bowlingScorer.js
+++ src/bowlingScorer.js
@@ -2,46 +2,61 @@ let actualScore = 0;
 let frame = 0;
 let turn = 0;
 let isGameOver;
-
+let table = [];
+let pins = [];
 /*
-{
-    frameId:0,
-    throwedPins: [0,0],
-    frameScore:0
-}
+  {
+    frameId: 0,
+    throwedPins: [0, 0],
+    frameScore: 0
+  }
 */
 
 let scoreTable = [];
 
 function newGame() {
 	actualScore = 0;
-	frame = 1;
+	frame = 0;
 	turn = 0;
 	isGameOver = false;
 }
 
-function throwedPins(count) {
-	if (frame == 10) isGameOver = true;
-
-	while (!isGameOver) {
-		if (turn == 2) {
-			frame += 1;
-			turn = 1;
-		} else {
-			turn += 1;
-		}
-
-		scoreTable[frame] = {
-			frameId: frame,
-			throwedPins: [
-				...(scoreTable[frame] && scoreTable[frame]["throwedPins"]
-					? scoreTable[frame]["throwedPins"]
-					: []),
-				count,
-			],
-			frameScore: count,
-		};
+function gameOver() {
+	if (frame >= 10 && turn == 2) {
+		!isGameOver;
+		alert("game over");
+		return;
 	}
+}
+
+function throwedPins(count) {
+	// dummy logic
+	if (turn === 1 && count === 10) {
+		count = "X";
+		//spare
+	}
+	if (turn === 0 && count === 10) {
+		count = "X";
+	}
+	if (turn === 1) {
+		pins = [...pins, count];
+		updateTable(pins);
+		turn = 0;
+	}
+
+	if (turn === 0) {
+		pins = [count];
+		turn++;
+	}
+}
+
+function updateTable(pins) {
+	table[frame] = {
+		frameId: frame,
+		throwedPins: pins,
+	};
+	console.log(table);
+	frame++;
 }
 
 function getActualState() {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
-// import { newGame, throwedPins, getActualState } from "./bowlingScorer.js";
+import { newGame, throwedPins, getActualState } from "./bowlingScorer.js";
 
-console.log("string");
-// newGame();
-// throwedPins(1);
-// // throwedPins(6);
-// // throwedPins(6);
-// // throwedPins(6);
-// // throwedPins(10);
-// // throwedPins();
+newGame();
+throwedPins(1);
+throwedPins(2);
+throwedPins(3);
+throwedPins(10);
+throwedPins(6);
+throwedPins(10);
+// throwedPins(3);
+// throwedPins(5);
+
 // console.log(getActualState());
